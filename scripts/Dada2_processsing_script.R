5bb4f51ec5ee4dbaf3cec90a447be61b91288113
Grace Klinges <27785052+graceklinges@users.noreply.github.com>
2020-02-13 18:13:51 -0800

    Update Dada2_processsing_script.R

--- scripts/Dada2_processsing_script.R
+++ scripts/Dada2_processsing_script.R
@@ -1,11 +1,13 @@
 library(dada2); packageVersion("dada2")
 
+# Location of raw reads
 pathF <- "~/Bleaching_Rickettsiales/fastq_filter/raw/F"
 pathR <- "~/Bleaching_Rickettsiales/fastq_filter/raw/R"
 
 # Forward and reverse fastq filenames have format: SAMPLENAME_R1.fq and SAMPLENAME_R2.fq
 fnFs <- sort(list.files(pathF, pattern="_R1.fq", full.names = TRUE))
 fnRs <- sort(list.files(pathR, pattern="_R2.fq", full.names = TRUE))
+
 # Extract sample names, assuming filenames have format: SAMPLENAME_XXX.fastq
 sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 1)
 plotQualityProfile(fnFs[1:2])
@@ -34,32 +36,43 @@ errF <- learnErrors(filtFs, nbases=1e8, multithread=TRUE)
 # Learn reverse error rates
 errR <- learnErrors(filtRs, nbases=1e8, multithread=TRUE)
 
+# If you like, visualize these error rates. You want the estimated error rates (black lines) to be a good 
+# fit to the observed error rates (points) and for the error rates drop with increased quality
+# plotErrors(errF, nominalQ=TRUE)
+# plotErrors(errR, nominalQ=TRUE)
+
 # Sample inference and merger of paired-end reads
 mergers <- vector("list", length(sample.names))
 names(mergers) <- sample.names
 
-#note to self: if this loop doesn't work, you've done something wrong. Stop trying to run it without the for loop. You've probably made an error earlier in the code, probably with the location of your forward and reverse reads and it's generated duplicate sample names
-#normal to get warning message that there are duplicate sequences
+# Note: if this loop doesn't work, you've done something wrong. You've probably made an error earlier in the code, 
+# Probably with the location of your forward and reverse reads and it's generated duplicate sample names
+# Normal to get warning message that there are duplicate sequences
 for(sam in sample.names) {
   cat("Processing:", sam, "\n")
   derepF <- derepFastq(filtFs[[sam]])
-  ddF <- dada(derepF, err=errF, multithread=TRUE)
+  ddF <- dada(derepF, err=errF, multithread=TRUE) # core sample interference algorithm
   derepR <- derepFastq(filtRs[[sam]])
-  ddR <- dada(derepR, err=errR, multithread=TRUE)
-  merger <- mergePairs(ddF, derepF, ddR, derepR)
+  ddR <- dada(derepR, err=errR, multithread=TRUE) # core sample interference algorithm
+  merger <- mergePairs(ddF, derepF, ddR, derepR) # Merge paired end reads
   mergers[[sam]] <- merger
 }
 
-st.all <- makeSequenceTable(mergers) #normal to get warning message saying the sequences being tabled vary in length
+# Make sequence table from merged reads
+st.all <- makeSequenceTable(mergers) # Normal to get warning message saying the sequences being tabled vary in length
+# Remove any ASVs that are considerably shorter or longer than target length of 292
 seqtab_trimmed <- st.all[,nchar(colnames(seqtab_strict)) %in% seq(290,295)]
 table(nchar(getSequences(seqtab_trimmed)))
 seqtab_trimmed_clean <- removeBimeraDenovo(seqtab_trimmed, method="consensus", multithread=TRUE)
 saveRDS(seqtab_trimmed_clean, "~/Bleaching_Rickettsiales/seqtab_trimmed_clean.rds")
 
+# Assign taxonomy based on silva reference database at genus level
 tax_silva <- assignTaxonomy(seqtab_trimmed_clean, "~/Bleaching_Rickettsiales/silva_nr_v132_train_set.fa.gz", multithread=TRUE)
+
+# Assign taxonomy based on silva reference database at species (100%) level
 silva_sp <- addSpecies(tax_silva, "~/Bleaching_Rickettsiales/silva_species_assignment_v132.fa.gz")
-ps_object <- phyloseq(otu_table(seqtab, taxa_are_rows=FALSE), 
-               tax_table(tax_silva))
-ps_object_sp <- phyloseq(otu_table(seqtab, taxa_are_rows=FALSE), 
-                             tax_table(silva_sp))
+
+# Export sequence table with genus and species assignments as phyloseq objects
+ps_object <- phyloseq(otu_table(seqtab_trimmed_clean, taxa_are_rows=FALSE), tax_table(tax_silva))
+ps_object_sp <- phyloseq(otu_table(seqtab_trimmed_clean, taxa_are_rows=FALSE), tax_table(silva_sp))
 
